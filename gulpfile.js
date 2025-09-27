f508364e55d8dc29e736618e15f2dfd7c53585c9
HallowDem <75336799+Cytrogen@users.noreply.github.com>
2025-09-26 22:54:29 -0400

    Update project configuration and build setup

--- gulpfile.js
+++ gulpfile.js
@@ -5,6 +5,7 @@ const cleanCSS = require('gulp-clean-css');
 const rename = require('gulp-rename');
 
 function compileSass() {
+    console.log('🎨 编译SCSS...');
     return gulp.src('./source/scss/*.scss')
         .pipe(sass({
             outputStyle: 'expanded'
@@ -13,13 +14,28 @@ function compileSass() {
         .pipe(gulp.dest('./source/css'))
         .pipe(cleanCSS())
         .pipe(rename({ suffix: '.min' }))
-        .pipe(gulp.dest('./source/css'));
+        .pipe(gulp.dest('./source/css'))
+        .on('end', () => console.log('✅ SCSS编译完成！BrowserSync会自动刷新页面'));
 }
 
 function watchFiles() {
-    gulp.watch(['./source/scss/**/*.scss'], compileSass);
+    console.log('👀 开始监听SCSS文件变化...');
+    console.log('💡 提示：请确保在另一个终端中运行 "hexo server" 以启用热加载');
+    gulp.watch(['./source/scss/**/*.scss'], gulp.series(compileSass));
+}
+
+function buildProd() {
+    return gulp.src('./source/scss/*.scss')
+        .pipe(sass({
+            outputStyle: 'compressed'
+        }).on('error', sass.logError))
+        .pipe(autoprefixer())
+        .pipe(cleanCSS({ level: 2 }))
+        .pipe(rename({ suffix: '.min' }))
+        .pipe(gulp.dest('./source/css'));
 }
 
 exports.sass = compileSass;
 exports.watch = watchFiles;
+exports.build = buildProd;
 exports.default = gulp.series(compileSass, watchFiles);
